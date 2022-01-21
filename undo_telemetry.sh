93c69306e73f36b347fb2249ea44ad2a5f166c70
bigbenzh <85002534+bigbenzh@users.noreply.github.com>
2022-01-21 20:24:11 +0800

    Update undo_telemetry.sh

--- undo_telemetry.sh
+++ undo_telemetry.sh
@@ -1,6 +1,9 @@
 # dc.services.visualstudio.com
 # vortex.data.microsoft.com
-TELEMETRY_URLS="(dc\.services\.visualstudio\.com)|(vortex\.data\.microsoft\.com)"
+TELEMETRY_URLS="(dc\.services\.visualstudio\.com)|(vortex\.data\.microsoft\.com)|(ticino\.blob\.core\.windows\.net)|(www\.microsoft\.com)|(api\.esrp\.microsoft\.com)|(dc\.services\.visualstudio\.com)|(dc\.applicationinsights\.microsoft\.com)|(dc\.applicationinsights\.azure\.com)|(global\.in\.ai\.monitor\.azure\.com)|(global\.in\.ai\.privatelink\.monitor\.azure\.com)|(dc\.trafficmanager\.net)|(weu05-breeziest-in\.cloudapp\.net)|(experimentation\.visualstudio\.com)|(code\.visualstudio\.com)|(current\.cvd\.clamav\.net)"
+
+TELEMETRY_URLS=${TELEMETRY_URLS}"|(img\.shields\.io)|(vsmarketplacebadge\.apphb\.com)|(img\.shields\.io)|(badge\.buildkite\.com)|(raw\.githubusercontent\.com)|(api\.codeclimate\.com)|(codeclimate\.com)"
+
 REPLACEMENT="s/$TELEMETRY_URLS/0\.0\.0\.0/g"
 
 #include common functions
